3d44da7a06983804cf8926e5bfcd96c66f567321
0xurb <urbadeil@gmail.com>
2024-10-02 19:24:57 +0100

    feat: RPC request types

--- src/rpc.rs
+++ src/rpc.rs
@@ -1,1 +1,14 @@
+use std::path::PathBuf;
 
+use jsonrpsee::core::RpcResult;
+use tokio::sync::oneshot;
+
+/// RPC response sender representation
+pub type ResponseTx<T> = oneshot::Sender<RpcResult<T>>;
+
+#[derive(Debug)]
+pub enum RpcRequest {
+    ListPlugins { tx: ResponseTx<Vec<&'static str>> },
+    LoadPlugin { plugin_path: PathBuf, tx: ResponseTx<()> },
+    UnloadPlugin { id: &'static str, tx: ResponseTx<()> },
+}
